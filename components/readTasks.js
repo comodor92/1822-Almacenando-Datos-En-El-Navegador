223e2c394e554e046e8a96c21a60f2426b96e274
Jorge <jorgerobinson92@gmail.com>
2022-08-04 19:34:33 -0300

    clase07 completada

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,12 +1,22 @@
 import { createTask } from "./addTask.js";
+import dateElement from "./dateElement.js";
+import { uniqueDates } from "./services/date.js";
 
 export const readTasks = () => {
   const list = document.querySelector("[data-list]");
   console.log(list);
 
   const taskList = JSON.parse(localStorage.getItem('task')) || [];
+  const dates = uniqueDates(taskList);
+  console.log(dates);
 
-  taskList.forEach((task) => {
-    list.appendChild(createTask(task));
+  dates.forEach(date => {
+    const dateMoment = moment(date, "DD/MM/YYYY");
+    list.appendChild(dateElement(date));
+    taskList.forEach((task) => {
+      const taskDate = moment(task.dateFormat, "DD/MM/YYYY");
+      console.log(taskDate);
+      list.appendChild(createTask(task));
+    });
   });
 }
